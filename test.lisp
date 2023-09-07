4aab6927c39f5992caa4434c87497e6467618b86
FoolishDominator <1340995873@qq.com>
2023-09-07 20:01:44 +0800

    test

--- test.lisp
+++ test.lisp
@@ -1,2 +1,7 @@
-23
-(quote 23)
+((label myfunc (lambda (x) (cond
+                     ((atom x) x)
+                     ((quote t) (myfunc (car x)))))) (quote ((7 2) 3)) )
+
+                     (myfunc (label myfunc (lambda (x) (cond
+                     ((atom x) x)
+                     ((quote t) (myfunc (car x)))))))
